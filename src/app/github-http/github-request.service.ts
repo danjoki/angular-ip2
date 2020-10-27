dba9e78dcda8dacaa789af6c779e647fec7b960e
Daniel Njoroge Kinyanjui <dkinyanjui@safaricom.co.ke>
2020-10-27 12:06:34 +0300

    Fix on  GitHub Request Service

--- src/app/github-http/github-request.service.ts
+++ src/app/github-http/github-request.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GithubRequestService {
   constructor(private http: HttpClient) {}
+
+  //Get my GitHub user details
+  getGitHubInfo(): Observable<any> {
+    return this.http.get(
+      'https://api.github.com/users/danjoki?access_token=' + environment.apiKey
+    );
+  }
+
+  //Get My Repo
+  getMyGitHubRepos(): Observable<any> {
+    return this.http.get('https://api.github.com/users/danjoki/repos');
+  }
 }
